09a999435dc391d989039fd3ab651301ff8a7b63
googjohn <120353235+googjohn@users.noreply.github.com>
2023-12-13 20:26:59 +0800

    aside layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 const sections = document.getElementsByTagName('section');
+const moreTrendNews = document.querySelector('.more-trend-news');
+const mTn = moreTrendNews.children
+const mtn = [...mTn];
 
 for (let ii = 0; ii < sections.length; ii++) ii % 2 === 0 ? sections[ii].style.backgroundColor = '#ffffff' : sections[ii].style.backgroundColor = '#eaeaea';
+
+for (let ii = 0; ii < mtn.length; ii++) {
+    const listItem = mtn[ii];
+    if (ii % 2 === 0) {
+        listItem.style.backgroundColor = '#eaeaea'
+    } else {
+        listItem.style.backgroundColor = '#a6a6a6'
+    }
+}

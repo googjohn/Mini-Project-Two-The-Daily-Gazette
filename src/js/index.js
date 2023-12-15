94da4c315a45371f1a05e34f88c2212ae721a2a7
googjohn <120353235+googjohn@users.noreply.github.com>
2023-12-15 14:43:27 +0800

    more layout

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,33 @@ const moreTrendNews = document.querySelector('.more-trend-news');
 const mTn = moreTrendNews.children
 const mtn = [...mTn];
 
+const moreEntertainmentNews = document.querySelector('.more-entertainment-news')
+const mEn = moreEntertainmentNews.children
+const men = [...mEn];
+
+const moreScitechNews = document.querySelector('.more-scitech-news')
+const mSn = moreScitechNews.children
+const msn = [...mSn];
+
+const moreSportsNews = document.querySelectorAll('.more-sports-news')
+const mSportn = moreSportsNews.forEach( ol => {
+    ol.style.paddingBottom = '20px';
+    listItemStyle(ol.children)
+});
+
 for (let ii = 0; ii < sections.length; ii++) ii % 2 === 0 ? sections[ii].style.backgroundColor = '#ffffff' : sections[ii].style.backgroundColor = '#eaeaea';
 
-for (let ii = 0; ii < mtn.length; ii++) {
-    const listItem = mtn[ii];
-    if (ii % 2 === 0) {
-        listItem.style.backgroundColor = '#eaeaea'
-    } else {
-        listItem.style.backgroundColor = '#a6a6a6'
+function listItemStyle(arr) {
+    for (let ii = 0; ii < arr.length; ii++) {
+        const listItem = arr[ii];
+        if (ii % 2 === 0) {
+            listItem.style.backgroundColor = '#eaeaea';
+        } else {
+            listItem.style.backgroundColor = '#a6a6a6'
+        }
     }
 }
+
+listItemStyle(mtn)
+listItemStyle(men)
+listItemStyle(msn)
